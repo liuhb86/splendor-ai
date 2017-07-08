b377f074a123b053a94aeab82d5b3a0c8c5c2b60
Hongbo Liu <liuhb86@gmail.com>
2017-07-08 13:18:57 -0400

    add most valuable card to state evaluation

--- app/com/simplex9/splendor/valueestimator/StateEvaluator.scala
+++ app/com/simplex9/splendor/valueestimator/StateEvaluator.scala
@@ -10,22 +10,24 @@ object StateEvaluator {
     var maxOpponent = -Param.INF
     for (i <- state.players.indices) {
       if (i != iPlayer) {
-        val score = evaluatePlayer(state.players(i))
+        val score = evaluatePlayer(state.players(i), state)
         if (score > maxOpponent) maxOpponent = score
       }
     }
-    evaluatePlayer(state.players(iPlayer)) - maxOpponent
+    evaluatePlayer(state.players(iPlayer), state) - maxOpponent
   }
 
-  def evaluatePlayer(player: Player): Int = {
-    if (player.points >= 15) {
+  def evaluatePlayer(player: Player, state: State): Int = {
+    if (player.points >= Param.WINNING_POINTS) {
       return Param.WINNING_SCORE + player.points * Param.POINT_VALUE - player.cards.sum
     }
 
+    val valueEstimator = new ValueEstimator(state, player)
     player.points * Param.POINT_VALUE +
       player.coins.sum * Param.COIN_VALUE +
       player.cards.sum * Param.CARD_VALUE +
-      player.golds * Param.GOLD_VALUE
+      player.golds * Param.GOLD_VALUE +
+      valueEstimator.cardEstimator.values.head.value
   }
 
 }
